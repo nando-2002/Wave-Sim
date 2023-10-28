95a300b2a324323a62b05eaae29ad29ac472d8e4
nando <prthik.kar@gmail.com>
2023-10-28 17:46:12 +0530

    working 2d

--- 2D_FiniteDiff_Wave.py
+++ 2D_FiniteDiff_Wave.py
@@ -4,7 +4,7 @@ from matplotlib import cm
 
 nx = 1000
 ny = 1000
-nt = 10000
+nt = 9000
 
 L = 1 #metres
 c = 2 #metres per second
@@ -21,13 +21,19 @@ u_old = np.zeros([nx, ny])
 u_now = np.zeros([nx, ny])
 u_future = np.zeros([nx, ny])
 
-#input initial conditionsz
-u_old[int(3*nx/8):int(5*nx/8),int(3*ny/8):int(5*ny/8)] = 0.5
+#input initial conditions
+A = 4 #wave height - kinda not really
+B = int(1*nx/8) #wave pos
+C = 25 #wave width - again not really but kinda
+
+for xx in range(nx):
+    for yy in range(ny):
+        u_old[xx, yy] = A*np.exp(-((xx - B)**2 + (yy - B)**2)/(2*C**2))
 u_now = u_old
 
 def display(output):
     plt.clf()
-    plt.pcolormesh(x, y, output, shading='auto', cmap='viridis')
+    plt.pcolormesh(x, y, output, shading='auto', cmap='viridis', vmax=4.2, vmin=-4.2)
     plt.colorbar()
     plt.ylim(0, L)
     plt.xlim(0, L)
@@ -35,8 +41,15 @@ def display(output):
     plt.xlabel('X')
     plt.ylabel('Y')
     plt.pause(0.001)
+    
+def otherdisplay(output):
+    fig, ax = plt.subplots(subplot_kw = {"projection":"3d"})
+    a = x
+    b = y
+    a, b = np.meshgrid(a, b)
+    surf = ax.plot_surface(a, b, output, cmap = cm.viridis, linewidth = 0, antialiased = False)
 
-display(u_old)
+#otherdisplay(u_old)
 
 for i in range(nt):
     u_future[1:nx-1, 1:ny-1] = (
@@ -49,5 +62,7 @@ for i in range(nt):
     u_old = u_now
     u_now = u_future
     u_future = u_old
-    if (i%1000 == 0):
-        display(u_future)
+    #if (i%1000 == 0):
+       
+otherdisplay(u_future)
+
